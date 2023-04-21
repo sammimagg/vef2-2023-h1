a4299aa43ad2fed94ac07ab4bd55768cc47f157d
sammimagg <sammimagg@gmail.com>
2023-04-21 23:42:39 +0000

    test

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
 
-import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 const app = express();
 
 // Allow all origins to make requests to your server
-app.use(cors());
+app.use();
 
 // Your routes and middleware come here
 
 // Error handling middleware
-app.use((err: Error,   req: Request,  res: Response,  next: NextFunction) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
+
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
